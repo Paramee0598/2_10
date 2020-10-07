cee94643acf1b359520f52c8f3edf04f33ad37ce
paramee0598 <63010598@kmitl.co.th>
2020-10-08 02:12:50 +0700

    เพิ่ม MAX & กรองตัวเลข

--- 2_10/Source.cpp
+++ 2_10/Source.cpp
@@ -1,27 +1,55 @@
 #include <stdio.h>
-void main(){
+#include <stdlib.h>
 
-	int i, j, total = 0;
-	int sc[3][3] = {};
+void line() {
+	printf("===================================================\n");
+}
+void chack(int score) {
+	if((score < 0)|(score > 50)) 
+	{
+		printf("ERROR!");
+		exit(0);
+	}
+	else if (!(score >= 0) | !(score <= 50))
+	{
+		printf("ERROR!");
+		exit(0);
+	}
+	
+}
+int main(){
 
-	printf("Input score of Calculus , ITC and ProFun \n\n");
+	int i, j, total = 0 ;
+	int sc[3][3] ;
+
+	printf("Input score of Match Unit 1 , Unit 2 and Unit 3 \n\n");
 
 	for (i = 0; i < 3; i++)
 	{
 		printf(" Student No.%2d\n", i + 1);
-		scanf_s("%d %d %d", &sc[i][0], &sc[i][1], &sc[i][2]);
+		printf(" Unit 1 (Max 50) :");
+		scanf_s("%d", &sc[i][0]); 
+		chack(sc[i][0]);
+		printf(" Unit 2 (Max 50) :");
+		scanf_s("%d", &sc[i][1]);
+		chack(sc[i][1]);
+		printf(" Unit 3 (Max 50) :");
+		scanf_s("%d", &sc[i][2]);
+		chack(sc[i][2]);
+
 	}
 	//**************************************************************
 
-	printf("\n\nSeq.   Cal    ITC    ProFun    Total\n");
-	printf("========================================\n");
+	printf("\n\nUNIT.   Unit 1 | Unit 2 | Unit 3 |   Total (Max 150)\n");
+	line();
 
 	for (i = 0; i < 3; i++)
 	{
 		total = sc[i][0] + sc[i][1] + sc[i][2];
 
-		printf("%2d. \t %2d\t %2d\t %2d\t %2d\n\n", i+1,sc[i][0], sc[i][1], sc[i][2],total);
+		printf("%2d. \t   %2d\t   %2d\t   %2d\t      %2d\n\n", i+1,sc[i][0], sc[i][1], sc[i][2],total);
+		line();
 	}
 	
-	//return(0);
+	return(0);
 }
